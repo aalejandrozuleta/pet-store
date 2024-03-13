fa4dd38d2ac9a9a4d9b1939893e74c40c2dc9ea7
aalejandrozuleta <jhoitan266@gmail.com>
2024-03-13 10:17:13 -0500

    primer commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+// importe el componente creado llamado catalogo
+import { CatalogoComponent } from './components/catalogo/catalogo.component';
+import { AboutComponent } from './components/about/about.component';
+AboutComponent
+
+// en el array routes incluir el objeto como esta definido a continuacion
+const routes: Routes = [
+  {
+    path: 'catalogo',
+    component: CatalogoComponent
+  },
+
+  {
+    path: 'about',
+    component: AboutComponent
+  }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CardProductComponent } from './components/card-product/card-product.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CatalogoComponent } from './components/catalogo/catalogo.component';
+import { AboutComponent } from './components/about/about.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardProductComponent,
+    HeaderComponent,
+    CatalogoComponent,
+    AboutComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
